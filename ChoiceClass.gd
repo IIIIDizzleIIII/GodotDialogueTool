2f08f6d256e6ddb1d423590765b99fd6cb7b3e66
Dizzle <IIIIDizzleIIII@hotmail.com>
2026-07-20 23:38:12 +0800

    Classes (+choices layout)

---
+++ ChoiceClass.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name DialogueChoice
+
+@export
+var ChoiceDialogue:String
+@export
+var NextEvent:DialogueEvent
+@export
+var NextScene:String
---
+++ DialogueClass.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name DialogueEvent
+
+@export_subgroup("EventDialogue")
+@export
+var speaker_name:String
+@export_multiline()
+var dialogue:String
+
+@export_subgroup("NextEvents")
+@export
+var next_node:DialogueEvent
+@export
+var dialogue_choices:Array[DialogueChoice]
+@export
+var next_scene:String
